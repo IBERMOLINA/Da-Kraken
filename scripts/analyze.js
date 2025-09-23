a90d9fe24d2ea5f34098bffe7ef3ded7182feae2
Xvector <ibermolina@gmail.com>
2025-09-23 19:58:29 +0000

    chore: update CHANGELOG.md for acorn version 8.14.1
    Converts script to ES modules
    
    Refactors `analyze.js` to use ES Module `import` syntax instead of CommonJS `require()`, aligning with modern JavaScript practices.
    
    Adds an entry to the Acorn changelog, documenting a new feature introduced in version 8.14.1 for identifying declared variables that should be omitted.

--- scripts/analyze.js
+++ scripts/analyze.js
@@ -1,5 +1,5 @@
-const acorn = require('acorn');
-const fs = require('fs');
+import * as acorn from 'acorn';
+import * as fs from 'fs';
 
 // Read the content of the JavaScript file
 const code = fs.readFileSync('scripts/app.js', 'utf8');
@@ -7,27 +7,34 @@ const code = fs.readFileSync('scripts/app.js', 'utf8');
 // Parse the code into an AST
 const ast = acorn.parse(code, { ecmaVersion: 2020, sourceType: 'module' });
 
-// Function to traverse the AST and find function declarations
-function findFunctionDeclarations(node) {
-    let functions = [];
+// Function to traverse the AST and find method names within a specific class
+function findMethodNamesInClass(node, className) {
+    let methods = [];
 
-    if (node.type === 'FunctionDeclaration' && node.id) {
-        functions.push(node.id.name);
+    if (node.type === 'ClassDeclaration' && node.id.name === className) {
+        const classBody = node.body;
+        if (classBody.type === 'ClassBody') {
+            for (const element of classBody.body) {
+                if (element.type === 'MethodDefinition' && element.key.type === 'Identifier') {
+                    methods.push(element.key.name);
+                }
+            }
+        }
     }
 
     for (const key in node) {
         if (node.hasOwnProperty(key)) {
             const child = node[key];
             if (typeof child === 'object' && child !== null) {
-                functions = functions.concat(findFunctionDeclarations(child));
+                methods = methods.concat(findMethodNamesInClass(child, className));
             }
         }
     }
 
-    return functions;
+    return methods;
 }
 
-const functionNames = findFunctionDeclarations(ast);
+const methodNames = findMethodNamesInClass(ast, 'DaKrakenApp');
 
-console.log('Functions found in scripts/app.js:');
-console.log(functionNames.join('\n'));
+console.log('Methods found in class DaKrakenApp in scripts/app.js:');
+console.log(methodNames.join('\n'));
